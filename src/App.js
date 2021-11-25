560b5667de2e8c9833e3584fefcf11c0b0ea022b
Jocelin-La-Roch <jocelinlaroch08@gmail.com>
2021-11-25 22:20:26 +0100

    add route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
 import HomePage from './Views/pages/HomePage';
+import LoginPage from "./Views/pages/LoginPage";
+import PrivateRoute from "./routes/PrivateRoute";
 
 function App() {
   return (
-    <HomePage/>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/login" component={LoginPage}/>
+          <Route exact path="/" render={() => (<Redirect to="/login"/>)}/>
+            <PrivateRoute exact path="/home" component={HomePage}/>
+        </Switch>
+      </BrowserRouter>
   );
 }
 
---
+++ src/routes/PrivateRoute.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Route, Redirect} from 'react-router-dom';
+
+const PrivateRoute = ({component: Component, ...rest}) =>{
+    const isLoggedIn = true;
+    return (
+        <Route
+            {...rest}
+            render={props =>
+                isLoggedIn ? (
+                    <Component {...props} />
+                ) : (
+                    <Redirect
+                        to={{
+                            pathname: "/login",
+                        }}
+                    />
+                )
+            }
+        />
+    );
+}
+
+export default PrivateRoute;
